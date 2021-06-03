b440bc1becf0268816fadcf649ebde2990ff2625
jgeng98 <77642029+jgeng98@users.noreply.github.com>
2021-06-02 22:44:25 -0400

    finish experiment function

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,5 +1,6 @@
 import copy
 import random
+from collections import Counter, defaultdict
 
 # Consider using the modules imported above.
 
@@ -11,9 +12,10 @@ class Hat:
             self.contents.extend([key] * value)
 
     def draw(self, num):
-        random.shuffle(self.contents)
-        drawn = self.contents[-num:]
-        self.contents = self.contents[:-num]
+        drawn = []
+        for i in range(min(num, len(self.contents))):
+            n = random.randint(0, len(self.contents) - 1)
+            drawn.append(self.contents.pop(n))
         return drawn
 
 
@@ -21,5 +23,16 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     successes = 0
 
     for i in range(num_experiments):
-        hat_copy = hat.copy()
+        hat_copy = copy.deepcopy(hat)
         balls_drawn = hat_copy.draw(num_balls_drawn)
+        success = True
+
+        for key, value in expected_balls.items():
+            if defaultdict(int, Counter(balls_drawn))[key] < value:
+                success = False
+                break
+
+        if success:
+            successes += 1
+
+    return successes / num_experiments
